b98c84991a7de9ca1c63361c78e70272f5bec860
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2019-01-09 23:38:05 -0200

    Adicionado componente para mostrar a lista de Pokemons

---
+++ src/components/PokemonListComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="pokemon of pokemonList" :key="pokemon.pokedex_entry">
+        {{pokemon.pokedex_entry}} - {{pokemon.id}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import PokemonService from '@/services/PokemonService'
+export default {
+    name: 'PokemonListComponent',
+    data() {
+      return {
+        pokemonList: []
+      }
+    },
+    methods: {
+      async getPokemonList() {
+        const response = await PokemonService.getPokemonList()
+        this.pokemonList = response.data
+        console.log(this.pokemonList)
+      }
+    },
+    mounted() {
+      this.getPokemonList()
+    }
+}
+</script>
+<style scoped>
+
+</style>
