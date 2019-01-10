dc3602a5660f5c9c901d7c20a8c9359dea5a36ac
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2019-01-09 23:40:03 -0200

    Lista os pokemons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <pokemon-list-component />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import PokemonListComponent from '@/components/PokemonListComponent'
 export default {
   name: 'home',
+
   components: {
-    HelloWorld
+    PokemonListComponent
   }
 }
 </script>
+<style scoped>
+
+</style>
