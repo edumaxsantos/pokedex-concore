a6a4806b452001bf16c5e733bd185ffb18c87004
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2019-01-11 11:23:36 -0200

    Adicionado buefy no Vue

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './../node_modules/bulma/css/bulma.css'
+import Buefy from 'buefy'
+//import './../node_modules/bulma/css/bulma.css'
+import 'buefy/dist/buefy.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy)
 
 new Vue({
   router,
