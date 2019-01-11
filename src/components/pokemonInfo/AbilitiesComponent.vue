cdfce5f42e95ba06528e5b69c694fa45f84d1e50
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2019-01-11 11:28:23 -0200

    Componente de habilidades

---
+++ src/components/pokemonInfo/AbilitiesComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="box">
+    <b-collapse class="box" :open="false">
+      <div slot="trigger" slot-scope="props" class="card-header">
+        <h4 class="card-header-title">Abilities</h4>
+        <a class="card-header-icon">
+          <b-icon
+              :icon="props.open ? 'menu-down' : 'menu-up'">
+          </b-icon>
+        </a>
+      </div>
+      <div class="card-content">
+        <div class="content">
+          <div class="card" v-for="ability of abilities" :key="abilities.indexOf(ability)">
+            <b-collapse class="card" :open="false">
+              <div slot="trigger" slot-scope="props" class="card-header">
+                <p class="card-header-title">
+                  {{ability.name}}
+                </p>
+                <a class="card-header-icon">
+                  <b-icon
+                      :icon="props.open ? 'menu-down' : 'menu-up'">
+                  </b-icon>
+                </a>
+              </div>
+              <div class="card-content">
+                <div class="content">
+                  <p>{{ability.text}}</p>
+              <p><b>Short Effect:</b> {{ability.short_effect}}</p>
+              <p><b>Effect:</b> {{ability.effect}}</p>
+                </div>
+              </div>
+            </b-collapse>
+          </div>
+        </div>
+      </div>
+    </b-collapse>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AbilitiesComponent',
+  props: ['abilities']
+}
+</script>
+<style scoped>
+  .hide {
+    display: none;
+  }
+</style>
