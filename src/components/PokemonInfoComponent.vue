ae3ba91b52d7d4ccbf2296ff487d1e828559b686
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2019-01-11 13:34:47 -0200

    separação dos componentes

--- src/components/PokemonInfoComponent.vue
+++ src/components/PokemonInfoComponent.vue
@@ -7,8 +7,15 @@
       <gallery-component :pokemonSprites="pokemonInfo.sprites"/>
       <div class="content">
         <div class="header">
-          <h1>{{pokemonInfo.id}} - {{capitalName(pokemonInfo.name)}}</h1>
-          <span v-for="type of pokemonInfo.types" :key="type" :class="['tag', type]">{{type}}</span>
+          <basic-info-component :basicInfo="basicInfo" />
+        </div>
+        <div class="box">
+          <div class="abilities">
+            <abilities-component :abilities="pokemonInfo.abilities"/>
+          </div>
+          <div class="moves">
+            <moves-component :moves="pokemonInfo.moves" />
+          </div>
         </div>
       </div>
     </div>
@@ -18,7 +25,9 @@
 <script>
 import PokemonService from '@/services/PokemonService'
 import GalleryComponent from '@/components/GalleryComponent'
-import capitalName from '@/capitalName'
+import BasicInfoComponent from '@/components/pokemonInfo/BasicInfoComponent'
+import AbilitiesComponent from '@/components/pokemonInfo/AbilitiesComponent'
+import MovesComponent from '@/components/pokemonInfo/MovesComponent'
 export default {
   name: 'PokemonInfoComponent',
   data() {
@@ -30,19 +39,26 @@ export default {
     pokemonInfo() {
       console.log(this.$store.state.pokemonInfo)
       return this.$store.state.pokemonInfo
+    },
+    basicInfo() {
+      return {
+        id: this.pokemonInfo.id,
+        name: this.pokemonInfo.name,
+        types: this.pokemonInfo.types,
+        weight: this.pokemonInfo.weight,
+        height: this.pokemonInfo.height,
+        base_experience: this.pokemonInfo.basic_experience 
+      }
     }
   },
   methods: {
     async getPokemonInfo(pokemonId) {
       this.loading = true
       return await PokemonService.getPokemonInfo(pokemonId)
-    },
-    capitalName(word) {
-      return capitalName(word)
     }
   },
   mounted() {
-    this.getPokemonInfo(this.$route.params.id).then(response => {
+    this.getPokemonInfo(this.$route.params.pokemonId).then(response => {
       console.log(response)
       this.$store.dispatch('savePokemonInfo', response.data)
       this.loading = false
@@ -51,85 +67,10 @@ export default {
       console.log(error)
     })
   },
-  components: {GalleryComponent}
+  components: {GalleryComponent, BasicInfoComponent, AbilitiesComponent, MovesComponent}
 }
 </script>
 <style scoped>
 
-.tag {
-  color: white;
-}
-
-.normal {
-  background-color:#A8A878;
-}
-
-.fire {
-  background-color: #F08030;
-}
-
-.water {
-  background-color: #6890F0;
-}
-
-.grass {
-  background-color: #78C850;
-}
-
-.electric {
-  background-color: #F8D030;
-}
-
-.ice {
-  background-color: #98D8D8;
-}
-
-.fighting {
-  background-color: #C03028;
-}
-
-.poison {
-  background-color: #A040A0;
-}
-
-.ground {
-  background-color: #E0C068;
-}
-
-.flying {
-  background-color: #A890F0;
-}
-
-.psychic {
-  background-color: #F85888;
-}
-
-.bug {
-  background-color: #A8B820;
-}
-
-.rock {
-  background-color: #B8A038;
-}
-
-.ghost {
-  background-color: #705898;
-}
-
-.dark {
-  background-color: #705848;
-}
-
-.dragon {
-  background-color: #7038F8;
-}
-
-.steel {
-  background-color: #B8B8D0;
-}
-
-.fairy {
-  background-color: #F0B6BC;
-}
 
 </style>
